32351adee544ea4cf8e1245b08378c2fc41848e9
Cassage <mavrin.2003@list.ru>
2023-10-20 10:58:48 +0300

    done done

---
+++ src/Facade/HomeControlSystem.java
@@ -0,0 +1,16 @@
+package Facade;
+
+public class HomeControlSystem {
+
+    AudioController audioController;
+    TempController tempController;
+    LightsController lightsController;
+
+    public HomeControlSystem() {
+        audioController = new AudioController();
+        tempController = new TempController();
+        lightsController = new LightsController();
+    }
+
+}
+
